3bbd41103e632ed3d3fb8160e443abf5b69f028a
jnatmed <49349742+jnatmed@users.noreply.github.com>
2022-01-06 02:30:46 -0300

    fin configuracion backend

---
+++ README.md
@@ -0,0 +1,2 @@
+Siguiendo tutorial del publicado en youtube
+https://www.youtube.com/watch?v=DqpL5UtJHus&t=2394s&ab_channel=Fazt
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+
+const { mongoose } = require('./database')
+
 const app = express();
-var router = express.Router();
 
 // Settings 
 app.set('port', process.env.PORT || 3000);
@@ -12,9 +15,10 @@ app.use(express.json());
 
 // Routes    
 const task_routes = require('./routes/task.routes.js');
-app.use('/task', task_routes);
+app.use('/api/task', task_routes);
 
 // Statics files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting the server 
 app.listen(app.get('port'), () => {
