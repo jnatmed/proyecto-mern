ac2a5ddbf317d618cb0e4c5069fa723a01db6315
jnatmed <49349742+jnatmed@users.noreply.github.com>
2022-01-07 03:00:14 -0300

    hello world con react

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/mern-task';
 
-mongoose.connect(URI)
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 
